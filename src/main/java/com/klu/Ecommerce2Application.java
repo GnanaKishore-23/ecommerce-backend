029be916d0455d3f9776a00144c3fba9b39637eb
GnanaKishore-23 <gnannakishorer23@gmail.com>
2026-03-26 14:12:26 +0530

    backend added

---
+++ src/main/java/com/klu/Ecommerce2Application.java
@@ -0,0 +1,14 @@
+package com.klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecommerce2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ecommerce2Application.class, args);
+		System.out.println("Application has been running..");
+	}
+
+}
